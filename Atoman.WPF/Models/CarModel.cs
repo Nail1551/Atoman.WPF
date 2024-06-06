1ddca67c4b4fed419feb3ad1c9eb13973877d222
Andrey Menkov <57506749+aamenkov@users.noreply.github.com>
2024-06-06 12:10:33 +0300

    Add files via upload

---
+++ Atoman.WPF/Models/CarModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atoman.WPF.Models
+{
+    public class CarModel
+    {
+        /// <summary>
+        /// Идентификатор автомобиля
+        /// </summary>
+        public Guid CarId { get; set; }
+        /// <summary>
+        /// Марка автомобиля
+        /// </summary>
+        public CarTypeModel CarTypeModel { get; set; }
+        /// <summary>
+        /// Пробег автомобиля
+        /// </summary>
+        public int CarMileage { get; set; }
+        /// <summary>
+        /// Гос.номер автомобиля
+        /// </summary>
+        public string CarNumber { get; set; }
+        /// <summary>
+        /// Штрих-код автомобиля
+        /// </summary>
+        public string CarBarCode { get; set; }
+
+    }
+
+    public static class CarMockService
+    {
+        public static List<CarModel> GetCars()
+        {
+            return new List<CarModel>()
+            {
+                new CarModel
+                {
+                        CarId = Guid.NewGuid(),
+                        CarBarCode = string.Empty,
+                        CarMileage = 0,
+                        CarNumber = "T538OO790",
+                        CarTypeModel = new CarTypeModel
+                        {
+                            CarTypeId = Guid.NewGuid(),
+                            CarTypeName = "Porche Cayene",
+                            InspectionPatternId = 1
+                        }
+                },
+                new CarModel
+                {
+                        CarId = Guid.NewGuid(),
+                        CarBarCode = string.Empty,
+                        CarMileage = 0,
+                        CarNumber = "K753AM790",
+                        CarTypeModel = new CarTypeModel
+                        {
+                            CarTypeId = Guid.NewGuid(),
+                            CarTypeName = "Гелик",
+                            InspectionPatternId = 2
+                        }
+                }
+            };
+        }
+    }
+
+
+}
